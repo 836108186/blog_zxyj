71c43be83ee1ac3cda656b447d301d7a284cff86
836108186 <836108186@qq.com>
2025-10-13 14:15:59 +0800

    重构关于页面，提取 AboutPageContent 组件并优化导入

---
+++ app/(site)/about/AboutPageContent.tsx
@@ -0,0 +1,29 @@
+import { Authors, allAuthors } from 'contentlayer/generated'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { coreContent } from 'pliny/utils/contentlayer'
+import { Locale, getDocumentLocale } from '@/lib/i18n'
+
+import { ABOUT_TITLES } from './metadata'
+
+export function AboutPageContent({ locale }: { locale: Locale }) {
+  const author = (allAuthors.find(
+    (p) => p.slug === 'default' && getDocumentLocale(p.lang) === locale
+  ) ??
+    allAuthors.find((p) => p.slug === 'default') ??
+    allAuthors[0]) as Authors | undefined
+
+  if (!author) {
+    throw new Error('Author profile "default" is missing. Please add data/authors/zh/default.mdx.')
+  }
+
+  const mainContent = coreContent(author)
+
+  return (
+    <>
+      <AuthorLayout content={mainContent} title={ABOUT_TITLES[locale]}>
+        <MDXLayoutRenderer code={author.body.code} />
+      </AuthorLayout>
+    </>
+  )
+}
--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,38 +1,12 @@
-import { Authors, allAuthors } from 'contentlayer/generated'
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
-import { genPageMetadata } from 'app/seo'
-import { DEFAULT_LOCALE, Locale, getDocumentLocale, resolveLocaleParam } from '@/lib/i18n'
-
-import { ABOUT_TITLES, createAboutMetadata } from './metadata'
-
+import { DEFAULT_LOCALE, resolveLocaleParam } from '@/lib/i18n'
+import { AboutPageContent } from './AboutPageContent'
+import { createAboutMetadata } from './metadata'
 export const metadata = createAboutMetadata()
 
 type AboutPageParams = { params?: { locale?: string } }
 
-function AboutPageContent({ locale }: { locale: Locale }) {
-  const author = (allAuthors.find(
-    (p) => p.slug === 'default' && getDocumentLocale(p.lang) === locale
-  ) ??
-    allAuthors.find((p) => p.slug === 'default') ??
-    allAuthors[0]) as Authors | undefined
-  if (!author) {
-    throw new Error('Author profile "default" is missing. Please add data/authors/zh/default.mdx.')
-  }
-  const mainContent = coreContent(author)
-  return (
-    <>
-      <AuthorLayout content={mainContent} title={ABOUT_TITLES[locale]}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
-    </>
-  )
-}
 
 export default function Page({ params }: AboutPageParams = {}) {
   const locale = resolveLocaleParam(params, DEFAULT_LOCALE)
   return <AboutPageContent locale={locale} />
 }
-
-export { AboutPageContent }
